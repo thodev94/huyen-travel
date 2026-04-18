78ce96647c0539a7fe4d12d47f28eebee21138ee
thodev <thodo7188@gmail.com>
2026-04-18 21:49:12 +0700

    chore: remove tracked .next build artifacts

--- .next/static/chunks/app/tours/[id]/page-7416ea1d2d668a41.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[123],{5709:function(n,e,u){Promise.resolve().then(u.bind(u,9363))}},function(n){n.O(0,[224,403,363,253,961,744],function(){return n(n.s=5709)}),_N_E=n.O()}]);
