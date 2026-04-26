646407836261afbf3c605b982d34e1aacc4588f5
thodev <thodo7188@gmail.com>
2026-04-26 16:45:06 +0700

    update build

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -2,6 +2,20 @@ declare module "*.module.css" {
   const styles: { [className: string]: string };
   export = styles;
 }
+declare module "*.css" {
+  const content: { [className: string]: string } | string;
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.sass" {
+  const content: any;
+  export default content;
+}
 
 declare module "*.json" {
   const value: any;
